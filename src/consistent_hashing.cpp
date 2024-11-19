8eea8fc06c353d5066743e0db5c14b82947914c2
Carolina Gonçalves <carolina.cunha.gon@gmail.com>
2024-11-19 14:42:15 +0000

    added virtual nodes concept to ch

--- src/consistent_hashing.cpp
+++ src/consistent_hashing.cpp
@@ -14,9 +14,14 @@ private:
     std::map<size_t, std::string> ring; // <hash, server_name>
     std::vector<std::string> servers;
     int numberOfServers = 0;
+    int numberOfVirtualNodes;
 
 public:
-    ConsistentHashing() = default;
+    ConsistentHashing(int virtualNodes=3) : numberOfVirtualNodes(virtualNodes) {}
+     
+    int getNumberOfVirtualNodes() const {
+        return numberOfVirtualNodes;
+    }
 
     void addServer(const std::string& server) {
         if (std::find(servers.begin(), servers.end(), server) != servers.end()) {
@@ -27,8 +32,10 @@ public:
         servers.push_back(server);
         numberOfServers++; 
 
-        size_t hash = generateHash(server);
-        ring[hash] = server; 
+        for(int i = 0; i < numberOfVirtualNodes; i++) {
+           size_t hash = generateHash(server + std::to_string(i));
+            ring[hash] = server + ":" + std::to_string(i); 
+        }
     }
 
     void removeServer(const std::string& server) {
@@ -41,22 +48,35 @@ public:
         servers.erase(it);
         numberOfServers--;
 
-        size_t hash = generateHash(server);
-        ring.erase(hash);
+        for(int i = 0; i < numberOfVirtualNodes; i++) {
+            size_t hash = generateHash(server + std::to_string(i));
+            ring.erase(hash);
+        }
     }
 
-    std::string getServer(const std::string& key) {
-        if(ring.empty()) {
+    std::string getServer(const std::string &key) {
+        if (ring.empty()) {
             return "";
         }
 
         size_t hash = generateHash(key);
-        auto it = ring.lower_bound(hash); 
+        auto it = ring.lower_bound(hash);
+        if (it == ring.end()) {
+            it = ring.begin(); 
+        }
+        return it->second.substr(0, it->second.find(":")); 
+    }
 
-        if(it == ring.end()) {
-            it = ring.begin();
+    std::string getServerVirtual(const std::string &key) {
+        if (ring.empty()) {
+            return ""; 
         }
 
+        size_t hash = generateHash(key);
+        auto it = ring.lower_bound(hash);
+        if (it == ring.end()) {
+            it = ring.begin(); 
+        }
         return it->second;
     }
 
@@ -71,3 +91,15 @@ public:
     }
 
 }; 
+
+int main() {
+    ConsistentHashing ch(3);
+    ch.addServer("192.168.1.1:8080");
+    ch.addServer("192.168.1.2:8080");
+    ch.addServer("192.168.1.3:8080");
+
+    std::string key = "my_key";
+    std::cout << "Server for key: " << key << " -> " << ch.getServer(key) << std::endl;
+
+    return 0;
+}
