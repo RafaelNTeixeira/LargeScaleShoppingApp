3e50884b9a51767696493fd0e288a7f86d018b9b
Carolina Gonçalves <carolina.cunha.gon@gmail.com>
2024-12-12 16:16:10 +0000

    added send list to req handler

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -242,6 +242,9 @@ int main(int argc, char* argv[]) {
         std::string product_name = "";
         int product_quantity = 0;
 
+        json newShoppingListJson;
+        json currentShoppingListJson;
+
         while (!(std::cin >> choice)) {
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -258,7 +261,6 @@ int main(int argc, char* argv[]) {
                 std::cin >> list_name;
 
                 ShoppingList newShoppingList{list_id, list_name, full_url};
-                json newShoppingListJson;
                 to_json(newShoppingListJson, newShoppingList);
                 db.set(full_url, newShoppingListJson);
 
@@ -284,7 +286,7 @@ int main(int argc, char* argv[]) {
                     std::cout << "No shopping list found with that URL." << std::endl;
                     break;
                 }
-                json currentShoppingListJson = db.get(update_list_list_url);
+                currentShoppingListJson = db.get(update_list_list_url);
                 if (currentShoppingListJson.empty()) {
                     std::cerr << "Error: Shopping list not found." << std::endl;
                     break;
@@ -358,6 +360,10 @@ int main(int argc, char* argv[]) {
                     zmsg* msg = new zmsg();
                     msg->push_front(list_name.c_str());
                     msg->push_front(generated_url.c_str());
+                    std::string shoppingListStr = newShoppingListJson.dump();
+                    std::cout << "List: " << shoppingListStr << std::endl;
+                    msg->push_front(shoppingListStr.c_str());
+                    std::cout << "MSG SENT TO HANDLE op 1: " << std::endl;
                     client.send("LIST_MANAGEMENT", "CREATE_LIST", msg);
                     delete msg;
                 } else {
@@ -380,6 +386,9 @@ int main(int argc, char* argv[]) {
                     msg->push_front(std::to_string(product_quantity).c_str());
                     msg->push_front(product_name.c_str());
                     msg->push_front(update_list_list_url.c_str());
+                    std::string shoppingListStr = currentShoppingListJson.dump();
+                    msg->push_front(shoppingListStr.c_str());
+                    std::cout << "MSG SENT TO HANDLE op 3: " << std::endl;
                     client.send("LIST_MANAGEMENT", "UPDATE_LIST", msg);
                     delete msg;
                 } else {
